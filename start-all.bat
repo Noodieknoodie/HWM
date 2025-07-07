ba76e4e06596236ae235a2b0b6746de569f640e2
NoodieKnoodie <eriklars08@gmail.com>
2025-07-07 12:50:03 -0700

    working at work

--- start-all.bat
+++ start-all.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting Backend...
-start cmd /k "cd /d %~dp0backend && .venv\Scripts\activate && python -m uvicorn app.main:app --reload"
+start cmd /k "cd /d %~dp0backend && .venv-linux\Scripts\activate && python -m uvicorn app.main:app --reload"
 
 
 echo Starting Frontend...
